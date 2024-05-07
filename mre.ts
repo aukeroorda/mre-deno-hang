6c3d8d0f313141bff39ffc0906780189668b055d
Auke Roorda <aukeroorda@gmail.com>
2024-05-07 21:49:40 +0200

    Added comment on other method to import paper js, but this also causes the hang afterwards.

--- mre.ts
+++ mre.ts
@@ -2,15 +2,22 @@ if ("Deno" in window) {
   const paper_import = await import("npm:paper"); // Seems to create promises that never resolve, causing deno to not exit
   // const paper_import = await import("../node_modules/paper/dist/paper-full.js");
   const paper = paper_import.default;
-  // await import("npm:paper-jsdom");
-  // await import("../node_modules/paper-jsdom/index.js");
   globalThis.paper = paper;
   paper.setup();
-  console.log("setup paper");
 }
+
+// Or import as follows
+// import * as paper_import from "npm:paper";
+// const paper = paper_import.default;
+// globalThis.paper = paper;
+// paper.setup();
+console.log("setup paper");
 
 const pathdata = "M 0 0 C 0 10 10 10 10 0 z";
 console.log("Constructing path from:", pathdata);
+
+// NOTE This library call has causes Deno to hang. Some side-effects on the event loop? I don't know.
 const path = new paper.Path(pathdata);
 console.log({ path });
+
 console.log("Time to shutdown, Deno");
